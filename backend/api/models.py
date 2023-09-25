4e0163fa651404e703ba29164f80b0b848c96952
dizgen <dzigen@gmail.com>
2023-09-25 19:56:50 +0300

    make connection between api and db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class test(models.Model):
+
+    text_d = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "test"
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import test
 
 # Create your views here.
 
 class LandingAPIView(APIView):
     def get(self, request):
-        return Response({"message": "Hello from LandingAPIView"})
+        values = test.objects.all()[0].text_d
+        return Response({"message": "Hello from LandingAPIView", 'data': values})
 
 class UserSuggestionsAPIView(APIView):
     def get(self, request):
-        return Response({"message": "Hello from UserSuggestionsAPIView"})
+        values = test.objects.all()[0].text_d
+        return Response({"message": "Hello from UserSuggestionsAPIView", 'data': values})
